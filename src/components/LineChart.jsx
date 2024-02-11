28b534df20fb29e93185c454772bbd92b763847a
Will <rrozario958@gmail.com>
2024-02-12 02:20:56 +0530

    design changes 2

--- src/components/LineChart.jsx
+++ src/components/LineChart.jsx
@@ -4,7 +4,7 @@ import Chart from "chart.js/auto";
 
 const LineChart = ({ hourlyData }) => {
   const formatTimestamp = (timestamp) => {
-    const date = new Date(timestamp * 1000); // Convert seconds to milliseconds
+    const date = new Date(timestamp * 1000);
     const hours = date.getUTCHours().toString().padStart(2, "0");
     const minutes = date.getUTCMinutes().toString().padStart(2, "0");
 
